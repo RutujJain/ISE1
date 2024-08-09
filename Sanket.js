bc387d16d02959be0d7b457eafa4c8896b0ca12f
Rutuj Jain <rutujyambal2@gmail.com>
2024-08-09 15:16:11 +0530

    Sanket added

---
+++ Sanket.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sanket() {
+  return (
+    <div>
+      This js file is dedicated to  Mr.Sanket Jain from sangali
+    </div>
+  )
+}
+
+export default Sanket
